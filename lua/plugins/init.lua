f406dbff2b4a6ff1fba401af7ba958a1c15168aa
Saiful Islam <saiful.apm@gmail.com>
2021-10-26 11:59:47 +0600

    (fix) emment lazyload

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -187,16 +187,8 @@ return packer.startup(function()
     },
     {
       'mattn/emmet-vim',
-      ft = {
-        'html',
-        'css',
-        'javascript',
-        'javascriptreact',
-        'vue',
-        'typescript',
-        'typescriptreact',
-      },
-      config = function()
+      cmd = 'EmmetInstall',
+      setup = function()
         vim.g.user_emmet_complete_tag = 0
         vim.g.user_emmet_install_global = 0
         vim.g.user_emmet_install_command = 0
