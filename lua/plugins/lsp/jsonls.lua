fc32ad61c7708d8b60d443be86d8a98d00cdba24
Saiful Islam <saiful.apm@gmail.com>
2021-12-23 11:17:41 +0600

    (Chore) Lspconfig updated

--- lua/plugins/lsp/jsonls.lua
+++ lua/plugins/lsp/jsonls.lua
@@ -1,16 +1,14 @@
 local M = {}
 
-M.setup = function(_, capabilities)
+M.setup = function(on_attach, capabilities)
   require('lspconfig').jsonls.setup {
     settings = {
       json = {
         schemas = require('schemastore').json.schemas(),
       },
     },
+    on_attach = on_attach,
     capabilities = capabilities,
-    on_attach = function(client)
-      client.resolved_capabilities.document_formatting = false
-    end,
   }
 end
 
